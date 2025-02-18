4a1fc15f6a88490b3185aec76e77aac262cfc37d
Rodrigo <rlopezpatino@gmail.com>
2025-02-18 07:59:08 -0800

    Clipping

--- DuskToDawn/Assets/_Scripts/ClippingZone.cs
+++ DuskToDawn/Assets/_Scripts/ClippingZone.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class ClippingZone : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private List<GameObject> items;
+
+    /*private void OnTriggerStay(Collider other)
     {
-        
+        if (other.gameObject.tag == "Player)")
+        {
+            Debug.Log("Player");
+            for (int i = 0; i < items.Count; i++) 
+            {
+                items[i].transform.GetComponent<MeshRenderer>().enabled = false;
+            }
+        }
+    }*/
+
+    private void OnTriggerStay(Collider other)
+    {
+        //if the player touches the glass trap they will start to take damage per second while they are standing on the trap
+        if (other.tag == "Player")
+        {
+            Debug.Log("Player");
+            for (int i = 0; i < items.Count; i++)
+            {
+                items[i].transform.GetComponent<MeshRenderer>().enabled = false;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit(Collider other)
     {
-        
+        Debug.Log("Player");
+        for (int i = 0; i < items.Count; i++)
+        {
+            items[i].transform.GetComponent<MeshRenderer>().enabled = true;
+        }
     }
 }
