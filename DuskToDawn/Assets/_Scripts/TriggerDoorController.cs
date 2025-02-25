58763ce5f7cfe9f9faf7119f6743caaaf44e7581
mssalman11 <mssalman@csuchico.edu>
2025-02-25 09:34:45 -0800

    Basic Animation for one door

---
+++ DuskToDawn/Assets/_Scripts/TriggerDoorController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDoorController : MonoBehaviour
+{
+
+    [SerializeField]
+    private Animator doorAnim;
+
+    [SerializeField]
+    private bool openTrigger = false;
+    [SerializeField]
+    private bool closeTrigger = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")){
+            if (openTrigger)
+            {
+                doorAnim.Play("DoorOpen", 0, 0);
+            }
+            else if (closeTrigger)
+            {
+                doorAnim.Play("DoorClose", 0, 0);
+            }
+        }
+    }
+}
