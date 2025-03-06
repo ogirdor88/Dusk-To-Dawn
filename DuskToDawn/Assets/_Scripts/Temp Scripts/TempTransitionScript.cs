5122e62e513e5bf512e5eaf13468788475e43aa6
mssalman11 <mssalman@csuchico.edu>
2025-03-05 18:07:40 -0800

    Updated Build Settings to transition between scenes

---
+++ DuskToDawn/Assets/_Scripts/Temp Scripts/TempTransitionScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TempTransitionScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+    public void Playgame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
